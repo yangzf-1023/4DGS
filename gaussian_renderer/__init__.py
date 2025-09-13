3e6c429d3f196ec86abc251b0ede5d66146c06b8
yangzf-1023 <1258174135@qq.com>
2025-09-13 16:02:39 +0800

    添加新的factor衰减函数

--- gaussian_renderer/__init__.py
+++ gaussian_renderer/__init__.py
@@ -16,8 +16,8 @@ from .diff_gaussian_rasterization import GaussianRasterizationSettings, Gaussian
 from scene.gaussian_model import GaussianModel
 from utils.sh_utils import eval_sh, eval_shfs_4d
 
-def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
-           scaling_modifier = 1.0, override_color=None, args=None, curr_iter=0, until_iter=0, from_iter=0):
+def render(viewpoint_camera, pc: GaussianModel, pipe, bg_color: torch.Tensor, 
+           scaling_modifier=1.0, override_color=None, args=None, curr_iter=0, until_iter=0, from_iter=0):
     """
     Render the scene. 
     Background tensor (bg_color) must be on GPU!
@@ -64,12 +64,17 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor,
     if curr_iter > from_iter and args.opacity_decay:
         factor = args.opacity_decay_factor 
         if args.factor_decay:
-            k = 1
-            a = (1 - factor) / (math.exp(k) - 1)
-            b = factor - a
-            factor = a * math.exp(k * curr_iter / until_iter) + b
-            factor = min(1, factor)
-        opacity = pc.opacity_decay(factor=factor, mode=args.decay_mode, p=args.p, offset=args.offset)
+            if args.factor_decay_mode == 'exp':
+                k = args.k
+                a = (1 - factor) / (math.exp(k) - 1)
+                b = factor - a
+                factor = a * math.exp(k * curr_iter / until_iter) + b
+                factor = min(1, factor)
+            elif args.factor_decay_mode == 'sin':
+                factor = 1 - (1 - factor) * (math.sin(math.pi * curr_iter / until_iter) ** 2)
+            else:
+                raise NotImplementedError
+        opacity = pc.opacity_decay(factor=factor, mode=args.opacity_decay_mode, p=args.p, offset=args.offset)
 
     # If precomputed 3d covariance is provided, use it. If not, then it will be computed from
     # scaling / rotation by the rasterizer.
